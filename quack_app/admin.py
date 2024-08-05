40a6d29bda9be56bec690a6fd41cc3d0208513fd
Jay <joaomarcelo1015ja@gmail.com>
2024-08-05 19:48:27 -0300

    :sparkles: feat: Seguindo e Seguidores

--- quack_app/admin.py
+++ quack_app/admin.py
@@ -2,62 +2,65 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from quack_app.models import User, Post
 
+
 class MyUserAdmin(BaseUserAdmin):
     model = User
 
     fieldsets = (
-        (None, {'fields': ('handle', 'password')}),
+        (None, {"fields": ("handle", "password")}),
         (
-            'Informações Pessoais',
+            "Informações Pessoais",
             {
-                'fields': (
-                    'name',
-                    'email',
-                    'bio',
-                    'profile_pic',
-                    'location',
-                    'birth_date',
+                "fields": (
+                    "name",
+                    "email",
+                    "bio",
+                    "profile_pic",
+                    "location",
+                    "birth_date",
                 )
             },
         ),
         (
-            'Permissões',
+            "Permissões",
             {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
-                    'groups',
-                    'user_permissions',
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
                 )
             },
         ),
-        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+        ("Relações", {"fields": ("following", "followers")}),
+        ("Datas Importantes", {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
         (
             None,
             {
-                'classes': ('wide',),
-                'fields': (
-                    'handle',
-                    'name',
-                    'email',
-                    'password1',
-                    'password2',
-                    'profile_pic',
-                    'bio',
-                    'location',
-                    'birth_date',
+                "classes": ("wide",),
+                "fields": (
+                    "handle",
+                    "name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "profile_pic",
+                    "bio",
+                    "location",
+                    "birth_date",
                 ),
             },
         ),
     )
 
-    list_display = ('name', 'handle', 'email', 'is_staff')
-    search_fields = ('name', 'handle', 'email')
-    ordering = ('name',)
+    list_display = ("name", "handle", "email", "is_staff")
+    search_fields = ("name", "handle", "email")
+    ordering = ("name",)
+
 
 admin.site.register(User, MyUserAdmin)
 admin.site.register(Post)
