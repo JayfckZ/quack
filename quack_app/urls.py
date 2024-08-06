82e22b63b625e6875c7e753a6cb82b5a76dfeef0
Jay <joaomarcelo1015ja@gmail.com>
2024-08-06 15:16:36 -0300

    :sparkles: feat: Página de usuário

--- quack_app/urls.py
+++ quack_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    # path("profile/@<str:handle>/", views.profile, name="profile"),
+    path("profile/<str:handle>/", views.profile, name="profile"),
 ]
